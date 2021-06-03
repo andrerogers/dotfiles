939783cc47c8176a97a06d5b654105d351972a58
Andre Rogers <andrerogers@users.noreply.github.com>
2021-06-03 03:01:47 -0400

    Update emacs_fresh.el
    added dap mode

--- .cfg/emacs/emacs_fresh.el
+++ .cfg/emacs/emacs_fresh.el
@@ -295,6 +295,25 @@
   (lsp-format-buffer))
 (define-key global-map "\eF" 'format-buffer)
 
+;; DAP Mode
+(use-package dap-mode
+  ;; Uncomment the config below if you want all UI panes to be hidden by default!
+  ;; :custom
+  ;; (lsp-enable-dap-auto-configure nil)
+  ;; :config
+  ;; (dap-ui-mode 1)
+  :commands dap-debug
+  :config
+  ;; Set up Node debugging
+  (require 'dap-node)
+  (dap-node-setup) ;; Automatically installs Node debug adapter if needed
+
+  ;; Bind `C-c l d` to `dap-hydra` for easy access
+  (general-define-key
+    :keymaps 'lsp-mode-map
+    :prefix lsp-keymap-prefix
+    "d" '(dap-hydra t :wk "debugger")))
+
 ;; Company
 
 (use-package 
